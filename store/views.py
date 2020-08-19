3395f8a7e2ff5e0e4ca4bed4abe6881e68dfe159
mianamir <mianamirlahore@gmail.com>
2020-08-19 18:01:27 +0500

    adding daynamic functionality to the store, cart, checkout pages

--- store/views.py
+++ store/views.py
@@ -9,10 +9,33 @@ def store(request):
 
 
 def cart(request):
-    context = dict()
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete = False
+        )
+        items = order.orderitem_set.all()
+    else:
+        items = list()
+        order = {'get_cart_items': 0.0, 'get_cart_total': 0.0}
+
+    context = {'items': items, 'order': order}
+
     return render(request, "store/cart.html", context)
 
 
 def checkout(request):
-    context = dict()
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete = False
+        )
+        items = order.orderitem_set.all()
+    else:
+        items = list()
+        order = {'get_cart_items': 0.0, 'get_cart_total': 0.0}
+
+    context = {'items': items, 'order': order}
     return render(request, "store/checkout.html", context)
