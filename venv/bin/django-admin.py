9e49cb165ec2a15847578a4e3b58340e53c86457
mianamir <mianamirlahore@gmail.com>
2020-08-19 15:08:54 +0500

    adding ecommerce pages store, cart, checkout, models, products wih images

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/savvy/New Volume1/Learning/Python/django/2020_projects/ecommerce_app_django_3.0/ecommerce/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
