a8c67bcdd9dc1a2cb67abc59cd094c95ec4d3296
mianamir <mianamirlahore@gmail.com>
2020-09-15 20:00:27 +0500

    User app untrack files hot fix

---
+++ users_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            messages.success(request, f'Account created for {username} successfully!')
+            return redirect('blog-home')
+
+    else:
+        form = UserRegisterForm()
+    context = { 'form': form}
+    return render(request, 'users_app/register.html', context)
