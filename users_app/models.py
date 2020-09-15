78368abd52847aa00bc62c9545ee0355a9b4e062
mianamir <mianamirlahore@gmail.com>
2020-09-16 01:00:56 +0500

    User profile update, profile image resizing and author image on blog list page

--- users_app/models.py
+++ users_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from PIL import Image
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='user_profile_pics/default.jpg', upload_to='user_profile_pics')
@@ -9,3 +12,12 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
 
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 250 or img.width > 250:
+            size = (250, 250)
+            img.thumbnail(size)
+            img.save(self.image.path)
--- users_app/views.py
+++ users_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserRegisterForm
+from .forms import (UserRegisterForm, UserUpdateForm, ProfileUpdateForm)
 
 
 def register(request):
@@ -23,4 +23,26 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users_app/profile.html')
+
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, 
+                                        request.FILES,
+                                        instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been updated successfully. Now you can login!')
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'users_app/profile.html', context)
